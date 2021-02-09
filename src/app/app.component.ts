fa4f4ac469464b914013ecee1979bb9197494d09
CR0W666 <lukas.petracek@educanet.cz>
2021-02-09 18:54:40 +0100

    logic working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { LEADING_TRIVIA_CHARS } from '@angular/compiler/src/render3/view/template';
+
 import { Component } from '@angular/core';
 
 interface Card{
+  paired: boolean;
   icon: string;
   flipped: boolean;
 }
@@ -19,6 +20,11 @@ export class AppComponent {
   title = 'AngPex';
   difficulty = 5; // # of pairs
   cards: Card[] = []; //array of cards
+  flippedCards: Card[] = [];
+  foundPairs: Card[] = [];
+  won: boolean = false;
+  flips: number = 0;
+
 
   //-------------------- MAIN -----------------------
 
@@ -38,6 +44,10 @@ export class AppComponent {
 
   clean() { //cleans the card array
     this.cards = [];
+    this.flippedCards = [];
+    this.foundPairs = [];
+    this.won = false;
+    this.flips = 0;
   }
 
   fillArrayWithCards() { //fills the card array with cards
@@ -55,12 +65,99 @@ export class AppComponent {
 
   createCard(icon: string) { //returns card with specified icon
 
-    let newCard: Card = {icon: icon, flipped: false}; 
+    let newCard: Card = {paired: false, icon: icon, flipped: false}; 
     return newCard;
   }
 
 
   //-------------------- LOGIC -----------------------
+
+  async selected(card: Card) {
+    console.log(card);
+    this.flips++;
+    if(!this.hasBeenFound(card)) {
+      
+      this.flippedCards.push(card);
+      card.flipped = true;
+      this.how();
+      if(this.sameCardCheck()) {
+        await this.hide(false, 0);
+        this.flippedCards = [];
+        throw new Error("you clicked this card already");
+      } else {
+        if(this.hasPairSelected()) {
+            if(this.compareSymbols()) {
+              this.pairFound();
+              this.askIfWon();
+            } else {
+              await this.hide(true, 250);
+              this.flippedCards = [];
+            }
+        }
+      }
+
+
+    } else throw new Error("already found");
+
+
+
+  }
+
+  how() {
+    console.log("how???");
+    if(this.flippedCards.length > 1) {
+      if(this.flippedCards[2] != undefined) {
+        this.flippedCards[2].flipped = false;
+        this.flippedCards.pop();
+      }
+    }
+  }
+
+  askIfWon() {
+    if(this.foundPairs.length/2 == this.difficulty) {
+      this.won = true;
+    }
+  }
+
+  delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+  }
+
+  sameCardCheck() {
+    return this.flippedCards[0] == this.flippedCards[1];
+  }
+
+  hasPairSelected() {
+    return this.flippedCards.length == 2;
+  }
+
+  async hide(delay: boolean, delayTime: number) {
+    console.log("hiding");
+    if(delay) { await this.delay(delayTime); }
+    console.log(this.flippedCards);
+    if(this.flippedCards[0] != undefined) this.flippedCards[0].flipped = false;
+    if(this.flippedCards[1] != undefined) this.flippedCards[1].flipped = false;
+  }
+
+
+  compareSymbols() {
+    return this.flippedCards[0].icon == this.flippedCards[1].icon;
+  }
+
+  hasBeenFound(card: Card) {
+    return this.foundPairs.includes(card);
+  }
+
+  pairFound() {
+    this.foundPairs.push(this.flippedCards[0]);
+    this.foundPairs.push(this.flippedCards[1]);
+    this.flippedCards[0].paired = true;
+    this.flippedCards[1].paired = true;
+    this.flippedCards = [];
+  }
+
+
+
 
   
 
