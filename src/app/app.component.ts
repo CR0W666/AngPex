856c413fe61b1471a3939687f3af31ffe5f94528
CR0W <44005888+CR0W666@users.noreply.github.com>
2021-02-11 00:46:32 +0100

    comments and minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,9 +28,9 @@ export class AppComponent {
 
   //-------------------- MAIN -----------------------
 
-  startGame(num: number) {
+  startGame(num: string) {
     this.clean();
-    this.setDifficulty(num);
+    this.setDifficulty(num as unknown as number);
     this.fillArrayWithCards();
     this.cards = this.shuffleArray(this.cards);
     
@@ -51,112 +51,100 @@ export class AppComponent {
   }
 
   fillArrayWithCards() { //fills the card array with cards
-
     for (let i = 0; i < this.difficulty; i++) { //creates card pairs
-
       for(let x = 0; x < 2; x++) this.cards.push(this.createCard(i as unknown as string));
     }
   }
 
   shuffleArray(array: any[]) { //shuffles given array
-
     return array.sort(() =>  Math.random() - 0.5);
   }
 
   createCard(icon: string) { //returns card with specified icon
-
     let newCard: Card = {paired: false, icon: icon, flipped: false}; 
     return newCard;
   }
 
-
   //-------------------- LOGIC -----------------------
 
-  async selected(card: Card) {
-    this.flips++;
-    if(!this.hasBeenFound(card)) {
-      
-      this.flippedCards.push(card);
-      card.flipped = true;
-      this.how();
-      if(this.sameCardCheck()) {
-        await this.hide(false, 0);
-        this.flippedCards = [];
-        throw new Error("you clicked this card already");
+  async selected(card: Card) { //called onclick
+
+    this.flips++;                     //increments var that stores tries
+    if(!this.hasBeenFound(card)) {    //if card hasnt already been found
+      this.flippedCards.push(card);   //pushes selected card into array that stores uncovered//selected cards
+      card.flipped = true;            //sets this card as flipped
+      this.how();                     //some random bullshittery to handle flipping more than 2 cards
+      if(this.sameCardCheck()) {      //checks if new selected card is the same as the first one in the pair
+        await this.hide(false, 0);                        //if yes -> hide it
+        this.flippedCards = [];                           //-> empty the array
+        throw new Error("you clicked this card already"); //throw error
       } else {
-        if(this.hasPairSelected()) {
-            if(this.compareSymbols()) {
-              this.pairFound();
-              this.askIfWon();
+        if(this.hasPairSelected()) {        //if user selected a valid pair
+            if(this.compareSymbols()) {     //compares the symbols of cards
+              this.pairFound();                   //if the pair has the same symbol
+              this.askIfWon();                    //asks if the user found all pairs
             } else {
-              await this.hide(true, 250);
-              this.flippedCards = [];
+              await this.hide(true, 250);         //if the pair symbols are different hide the cards
+              this.flippedCards = [];             //and then empty the array
             }
         }
       }
-
-
-    } else throw new Error("already found");
-
-
-
+    } else throw new Error("already found");    //if the user clicks ona card that has been paired, throws error
   }
 
-  how() {
-    if(this.flippedCards.length > 1) {
-      if(this.flippedCards[2] != undefined) {
-        this.flippedCards[2].flipped = false;
-        this.flippedCards.pop();
-      }
+  how() {     //random bullshittery which somehow works??
+    if(this.flippedCards.length > 1) {    //if the user selected more then 2 cards
+      if(this.flippedCards[2] != undefined) {        //and that the third card exists
+        this.flippedCards[2].flipped = false;      //unflip third selected card
+        this.flippedCards.pop();                  //and remove it from the array of slected cards
+    }    //i should probably loop over cards with index higher then 1
     }
   }
 
-  askIfWon() {
+  askIfWon() {  //asks if the user has won
     if(this.foundPairs.length/2 == this.difficulty) {
       this.won = true;
     }
   }
 
-  delay(ms: number) {
+  delay(ms: number) {   //waits a specified amount of time
     return new Promise( resolve => setTimeout(resolve, ms) );
   }
 
-  sameCardCheck() {
+  sameCardCheck() {   //checks if the first selected card is the same as the second one
     return this.flippedCards[0] == this.flippedCards[1];
   }
 
-  hasPairSelected() {
+  hasPairSelected() {     //checks if a pair has been selected
     return this.flippedCards.length == 2;
   }
 
-  async hide(delay: boolean, delayTime: number) {
+  async hide(delay: boolean, delayTime: number) {  //hides the selected cards  (after waiting a little bit)
     if(delay) { await this.delay(delayTime); }
-    if(this.flippedCards[0] != undefined) this.flippedCards[0].flipped = false;
-    if(this.flippedCards[1] != undefined) this.flippedCards[1].flipped = false;
+
+    for(let i = 0; i < 2; i++) {
+      if(this.flippedCards[i] != undefined) this.flippedCards[i].flipped = false;
+    }
+
+    // if(this.flippedCards[0] != undefined) this.flippedCards[0].flipped = false;
+    // if(this.flippedCards[1] != undefined) this.flippedCards[1].flipped = false;
   }
 
 
-  compareSymbols() {
+  compareSymbols() {  //compares the symbols of selected cards
     return this.flippedCards[0].icon == this.flippedCards[1].icon;
   }
 
-  hasBeenFound(card: Card) {
+  hasBeenFound(card: Card) {  //checks if the selected card has been already paired
     return this.foundPairs.includes(card);
   }
 
-  pairFound() {
-    this.foundPairs.push(this.flippedCards[0]);
-    this.foundPairs.push(this.flippedCards[1]);
-    this.flippedCards[0].paired = true;
-    this.flippedCards[1].paired = true;
-    this.flippedCards = [];
+  pairFound() {     //called when a pair is found
+    for(let i = 0; i < 2; i++) {    //pushes both cards into array of paired cards and flags them as paired
+      this.foundPairs.push(this.flippedCards[i]);
+      this.flippedCards[i].paired = true;
+    }
+    this.flippedCards = []; //empties the array of selected cards
   }
-
-
-
-
-  
-
-
 
 }
